298c79d003cb5f178604aa2e1ed4b6679823dd60
Ekaterina Andriushova <ekaterinaandriushova@gmail.com>
2021-05-18 15:19:01 -0400

    changes

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,68 +1,36 @@
 import React, {Component} from 'react';
 import classes from './TrafficLight.module.css';
 
+const colors = ['green', 'yellow', 'red']
 
-const color = {
-  green: 0,
-  yellow: 1,
-  red: 2
-  }
+class Trafficlight extends Component {
+  state = { step: 0 }
 
-
-class  Trafficlight extends Component{
-  constructor(props) {
-    super(props);
-    this.state={
-      color: color.green
-    }
-  }
-
-  componentDidMount(){
-     const changeTrafficLight = () => {
-       if (this.state.color == color.green) {
-        this.setState({color: color.yellow})
-       }
-       else if (this.state.color == color.yellow) {
-        this.setState({color: color.red})
-       }
-       else if (this.state.color == color.red) {
-        this.setState({color: color.green})
-       }
+  changeTrafficLight = () => {
+    const step = this.state.step
+    return (step === 2) ? this.setState({ step: 0 }) : this.setState({ step: step + 1 })
      }
 
-   setInterval(changeTrafficLight, (20*1000)/3);
+  componentDidMount(){
+   setInterval(this.changeTrafficLight, (20*1000)/3);
   }
 
-  render(){
+   render(){
 
-  let backgroundColorClassName = classes.Container
-
-   switch(this.state.color){
-    case color.green:
-      backgroundColorClassName += ' ' + classes.Containergreen;
-      break;
-    case color.yellow:
-      backgroundColorClassName += ' ' + classes.Containeryellow;
-      break;
-    case color.red:
-     backgroundColorClassName += ' ' +classes.Containerred;
-      break;
-    default:
-     break;
-  }
-
-  let redLightClassName = classes.Light + (this.state.color == color.red ? ' ' + classes.Lightred : '')
-  let yellowLightClassName = classes.Light + (this.state.color == color.yellow ? ' ' + classes.Lightyellow : '')
-  let greenLightClassName = classes.Light + (this.state.color == color.green ? ' ' + classes.Lightgreen : '')
+    let currentColor = colors[this.state.step]
+    let backgroundColorClassName = classes.Container + ' ' + classes['Container' + currentColor]
+    let redLightClassName = classes.Light + (currentColor === 'red' ? ' ' + classes.Lightred : '')
+    let yellowLightClassName = classes.Light + (currentColor === 'yellow' ? ' ' + classes.Lightyellow : '')
+    let greenLightClassName = classes.Light + (currentColor === 'green' ? ' ' + classes.Lightgreen : '')
     return (
   <div>
        <div className={backgroundColorClassName}>
           <div className={classes.Trafficlight}>
-            <div className={redLightClassName}></div>
-            <div className={yellowLightClassName}></div>
-            <div className={greenLightClassName}></div>
+            <div className={redLightClassName} />
+            <div className={yellowLightClassName} />
+            <div className={greenLightClassName} />
           </div>
-          <p> {'This light is ' + (this.state.color == color.green ? 'green' : (this.state.color == color.yellow ? 'yellow' : 'red'))} </p>
+          <p>{`This light is ${currentColor}`}</p>
         </div>
   </div>
 );
